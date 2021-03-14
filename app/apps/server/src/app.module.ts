4bd8608b7b43fca4086cc0f39da61aa7314ba9cc
Gyanendro Kh <khgyanendro77@gmail.com>
2021-03-14 10:26:14 +0530

    Adding TypeOrm.

--- app/apps/server/src/app.module.ts
+++ app/apps/server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      url: process.env.DATABASE_URL,
+      synchronize: false,
+      logging: true,
+      entities: []
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
---
+++ app/ormconfig.js
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config(path.join(path.resolve(), '.env'));
+
+module.exports = {
+  type: 'mysql',
+  url: process.env.DATABASE_URL,
+  entities: ['apps/server/src/entity/*.entity.ts'],
+  migrationsTableName: 'migrations',
+  migrations: ['migrations/*.ts'],
+  cli: {
+    migrationsDir: 'migrations'
+  }
+};
