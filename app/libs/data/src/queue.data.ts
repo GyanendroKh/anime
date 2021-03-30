0b93ea3994d999c8aeecdc3d56dcb2b7c7885e21
Gyanendro Kh <khgyanendro77@gmail.com>
2021-03-30 15:34:52 +0530

    Adding data library.

---
+++ app/libs/data/src/queue.data.ts
@@ -0,0 +1,40 @@
+export class Queue<T> {
+  private data: T[];
+
+  constructor(data: T[] = []) {
+    this.data = data;
+  }
+
+  add(data: T): this {
+    this.data.push(data);
+    return this;
+  }
+
+  get(): T {
+    if (this.size() === 0) {
+      return null;
+    }
+
+    return this.data.shift();
+  }
+
+  peek(): T {
+    if (this.size() === 0) {
+      return null;
+    }
+
+    return this.data[0];
+  }
+
+  size(): number {
+    return this.data.length;
+  }
+
+  getData(): T[] {
+    return this.data;
+  }
+
+  remove(idx: number) {
+    this.data = this.data.filter((_, id) => id !== idx);
+  }
+}
