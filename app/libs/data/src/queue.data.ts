d17c4a1e04437efa34d92e6e8cf81e32cf69ef54
Gyanendro Kh <khgyanendro77@gmail.com>
2021-04-02 11:54:04 +0530

    (data) Adding listener for new data.

--- app/libs/data/src/queue.data.ts
+++ app/libs/data/src/queue.data.ts
@@ -1,5 +1,6 @@
 export class Queue<T> {
   private data: T[];
+  private onDataAdded: () => void = undefined;
 
   constructor(data: T[] = []) {
     this.data = data;
@@ -7,12 +8,30 @@ export class Queue<T> {
 
   add(data: T): this {
     this.data.push(data);
+
+    this.callOnAdded();
+
     return this;
   }
 
   addMultiple(data: T[]): this {
     this.data.push(...data);
+
+    this.callOnAdded();
+
     return this;
+  }
+
+  onAdded(callback: () => void): this {
+    this.onDataAdded = callback;
+
+    return this;
+  }
+
+  private callOnAdded() {
+    if (this.onDataAdded) {
+      this.onDataAdded();
+    }
   }
 
   get(): T {
