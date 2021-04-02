1352e4d22f698a18d905c26760157a6191e2cc91
Gyanendro Kh <khgyanendro77@gmail.com>
2021-04-02 16:47:07 +0530

    (spider) Cleaning Up.

--- app/apps/spider/src/spider.module.ts
+++ app/apps/spider/src/spider.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { SpiderController } from './spider.controller';
-import { SpiderService } from './spider.service';
 import { GoGoAnimeModule } from './go-go-anime/go-go-anime.module';
 
 @Module({
-  imports: [GoGoAnimeModule],
-  controllers: [SpiderController],
-  providers: [SpiderService]
+  imports: [GoGoAnimeModule]
 })
 export class SpiderModule {}
--- app/apps/spider/src/spider.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class SpiderService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
