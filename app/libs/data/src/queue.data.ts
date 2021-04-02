6f8619e343499025ce71af44edf3f1940193f722
Gyanendro Kh <khgyanendro77@gmail.com>
2021-04-02 16:22:58 +0530

    (data) Passing data on onAdded callback.

--- app/libs/data/src/queue.data.ts
+++ app/libs/data/src/queue.data.ts
@@ -1,6 +1,11 @@
+export type OnDataAdded<T> = (
+  type: 'single' | 'multiple',
+  data: T | T[]
+) => void;
+
 export class Queue<T> {
   private data: T[];
-  private onDataAdded: () => void = undefined;
+  private onDataAdded: OnDataAdded<T>;
 
   constructor(data: T[] = []) {
     this.data = data;
@@ -9,7 +14,7 @@ export class Queue<T> {
   add(data: T): this {
     this.data.push(data);
 
-    this.callOnAdded();
+    this.callOnAdded('single', data);
 
     return this;
   }
@@ -17,20 +22,20 @@ export class Queue<T> {
   addMultiple(data: T[]): this {
     this.data.push(...data);
 
-    this.callOnAdded();
+    this.callOnAdded('multiple', data);
 
     return this;
   }
 
-  onAdded(callback: () => void): this {
+  onAdded(callback: OnDataAdded<T>): this {
     this.onDataAdded = callback;
 
     return this;
   }
 
-  private callOnAdded() {
+  private callOnAdded(type: 'single' | 'multiple', data: T | T[]) {
     if (this.onDataAdded) {
-      this.onDataAdded();
+      this.onDataAdded(type, data);
     }
   }
 
