3deb0f183b7668a73b79a47ba17dd7423a733c6a
Gyanendro Kh <khgyanendro77@gmail.com>
2021-04-04 11:43:14 +0530

    (spider) Enabling cors.

--- app/apps/spider/src/main.ts
+++ app/apps/spider/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { SpiderModule } from './spider.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(SpiderModule);
+  const app = await NestFactory.create(SpiderModule, { cors: true });
+  app.enableCors({
+    origin: '*'
+  });
   await app.listen(3000);
 }
 bootstrap();
