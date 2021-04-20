cced936a142972d8f565b15e25d21ce503dd2bb7
Gyanendro Kh <khgyanendro77@gmail.com>
2021-04-20 19:05:01 +0530

    (mobile) Deleting sample data.

--- mobile/src/data.ts
+++
@@ -1,4 +0,0 @@
-import data from '../output.json';
-import { ISeries } from './types';
-
-export const getData = (): ISeries[] => data;
--- mobile/src/states/dataSource.ts
+++
@@ -1,8 +0,0 @@
-import { getData } from '../data';
-
-export const getSeriesInfo = (id: string) => {
-  const data = getData();
-  const anime = data.find(d => d.movieId === id);
-
-  return anime;
-};
