db97fe6ed1c832140721f2baf8a982990b451d7c
Gyanendro Kh <khgyanendro77@gmail.com>
2021-04-24 06:43:07 +0530

    (app) Fixing import path.

--- app/apps/server/src/pipe/pagination.pipe.ts
+++ app/apps/server/src/pipe/pagination.pipe.ts
@@ -1,6 +1,6 @@
 import { PipeTransform } from '@nestjs/common';
-import { IPaginatedQuery } from './types';
-import { checkPagination } from './utils';
+import { IPaginatedQuery } from '../types';
+import { checkPagination } from '../utils';
 
 export class PaginationDefaultPipe implements PipeTransform<IPaginatedQuery> {
   transform(data: IPaginatedQuery) {
