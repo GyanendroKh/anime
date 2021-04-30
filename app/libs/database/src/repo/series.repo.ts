eaf60e3ef3d955fcb36771031f808984dfb3b3d7
Gyanendro Kh <khgyanendro77@gmail.com>
2021-04-30 09:02:02 +0530

    (database) Adding method to get Series by Links.

--- app/libs/database/src/repo/series.repo.ts
+++ app/libs/database/src/repo/series.repo.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { IPaginatedQuery } from '../types';
-import { Series } from '../entity';
+import { GoGoAnimeSeries, Series } from '../entity';
 
 @Injectable()
 export class SeriesRepo {
   constructor(
     @InjectRepository(Series)
-    public repo: Repository<Series>
+    public repo: Repository<Series>,
+    @InjectRepository(GoGoAnimeSeries)
+    public gogoAnimeRepo: Repository<GoGoAnimeSeries>
   ) {}
 
   async list({ offset, limit }: IPaginatedQuery) {
@@ -28,5 +30,20 @@ export class SeriesRepo {
         relations: ['genres', 'episodes']
       }
     );
+  }
+
+  async getSeriesByLink(links: Array<string>) {
+    const gogoAnimeQuery = this.gogoAnimeRepo
+      .createQueryBuilder('gs')
+      .select('gs.series')
+      .where('gs.link IN (:...links)');
+
+    const res = await this.repo
+      .createQueryBuilder('s')
+      .where(`s.id IN (${gogoAnimeQuery.getQuery()})`)
+      .setParameter('links', links)
+      .getMany();
+
+    return res;
   }
 }
